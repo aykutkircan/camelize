f228c3d456d6a7cade07fbad8e43a934d8708231
Aykut Kırcan <aykutkircan@icloud.com>
2022-05-05 15:39:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### this fucntion is already exists in lodash
+
+[Here Document]('https://lodash.com/docs/4.17.15#camelCase')..
---
+++ index.js
@@ -0,0 +1,8 @@
+const { camelize } = require('./lib/camelize')
+
+// const str = 'hello world';
+
+// const result = camelize(str);
+// console.log(result)
+
+module.exports = { camelize };
